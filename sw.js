a74f893599c893d42736833d2afc5271a0719a3e
twokaif <tukaevwedding@gmail.com>
2026-04-24 13:46:32 +0300

    Replace Chopin with Unbounded + autosave draft to localStorage

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='ruzhnikov-png-v2';
-const ASSETS=['./','./index.html','./manifest.json','./fonts/HELVETICANEUECYR-BOLD.TTF','./fonts/Bebas Neue Cyrillic.ttf','./fonts/Chopin-Trial-SemiBold.ttf','./fonts/Felidae.ttf','./icon-192.png','./icon-512.png'];
+const CACHE='ruzhnikov-png-v3';
+const ASSETS=['./','./index.html','./manifest.json','./fonts/HELVETICANEUECYR-BOLD.TTF','./fonts/Bebas Neue Cyrillic.ttf','./fonts/Unbounded.ttf','./fonts/Felidae.ttf','./icon-192.png','./icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
